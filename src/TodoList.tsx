5bbd55f4db48f838d5adca6c5a8c46f5f6e95080
412 <85489519+laybacksound96@users.noreply.github.com>
2023-02-12 22:38:19 +0900

    #6.5 setup

---
+++ src/TodoList.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function TodoList() {
+  const [todo, setTodo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setTodo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(todo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={todo} placeholder="write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoList;
