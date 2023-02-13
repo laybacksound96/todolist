3752a28a2bf7316d64e7242c90ac16ec81582df2
412 <laybacksound96@gmail.com>
2023-02-13 13:29:30 +0900

    custom validation

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,23 +1,59 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  password1: string;
+}
 function ToDoList() {
-  const { register, watch, handleSubmit } = useForm();
-  const onVaild = (data: any) => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>();
+  const onVaild = (data: IForm) => {
     console.log(data);
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
   };
+
   return (
     <div>
       <form onSubmit={handleSubmit(onVaild)}>
         <input
-          {...(register("email"), { required: true })}
+          {...register("email", {
+            required: "Email is required.",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "Only naver.com emails allowed",
+            },
+          })}
           placeholder="Email"
         />
-        <input {...register("firstName")} placeholder="First Name" />
+        <span>{errors.email?.message}</span>
+        <input
+          {...register("firstName", {
+            required: "write here",
+            validate: (value) => (value.includes("nico") ? "No nico" : true),
+          })}
+          placeholder="First Name"
+        />
+        <span>{errors.firstName?.message}</span>
         <input {...register("lastName")} placeholder="Last Name" />
         <input {...register("username")} placeholder="Username" />
         <input {...register("password")} placeholder="Password" />
         <input {...register("password1")} placeholder="Password1" />
+        <span>{errors.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
